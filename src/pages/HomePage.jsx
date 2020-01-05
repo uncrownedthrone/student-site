4900d6d79fa5b0f44ea9d8b10b034a3aa54a8163
Justin Swiger <justin.swiger@me.com>
2020-01-05 14:26:07 -0500

    students showing on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const HomePage = () => {
-  return <HelloWorld />
+  const [students, setStudents] = useState([])
+  const getStudents = async () => {
+    const resp = await axios.get('https://localhost:5001/api/student')
+    setStudents(resp.data)
+  }
+
+  useEffect(() => {
+    getStudents()
+  }, [])
+  return (
+    <>
+      <header>All Students</header>
+      <ul>
+        {students.map(student => {
+          return (
+            <li>
+              <Link to={`/student/${student.id}`}>{student.fullName}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default HomePage
